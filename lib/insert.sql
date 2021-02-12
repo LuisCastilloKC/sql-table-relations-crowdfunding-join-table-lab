6aa6398198069e069d5d90c46e8f340a9ecb6859
Luis Castillo <raul@luiss-mbp.myfiosgateway.com>
2021-02-11 22:16:20 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,71 @@
+INSERT INTO 
+users (name, age) 
+VALUES 
+('Finnebar', 1), 
+('Finnebar', 1), 
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1),
+('Finnebar', 1);
+
+INSERT INTO 
+projects (title, category, funding_goal, start_date, end_date)
+VALUES
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30'),
+('Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30');
+
+INSERT INTO
+pledges (amount, user_id, project_id)
+VALUES
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2),
+(10.00, 1, 2);
